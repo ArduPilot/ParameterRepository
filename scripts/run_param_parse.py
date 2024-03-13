eebb293e7cc044c946117da4361106b4c2533cad
Willian Galvani <williangalvani@gmail.com>
2024-03-13 14:47:40 -0300

    process beta tags

--- scripts/run_param_parse.py
+++ scripts/run_param_parse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import Tuple
 import git
 import glob
 import os
@@ -7,13 +8,14 @@ import re
 import shutil
 import subprocess
 import tempfile
+from pathlib import Path
 
 from packaging import version
 
 class Groundskeeper:
     repository_url = 'https://github.com/ArduPilot/ardupilot'
     repository_name = repository_url.split('/')[-1]
-    tag_regex = re.compile(r'(?P<name>[A-z]+(\d+)?)-(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)?')
+    tag_regex = re.compile(r'(?P<name>[A-z]+(\d+)?)(-(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)?|-(?P<beta>beta))$')
     temp_folder = tempfile.mkdtemp()
 
     valid_name_map = {
@@ -33,6 +35,23 @@ class Groundskeeper:
     Repository: {self.repository_url}
 """)
 
+    def get_version_for_tag(self, tag) -> Tuple[int, int]:
+      if tag['matches']['major'] is not None:
+          return int(tag['matches']['major']), int(tag['matches']['minor'])
+      else:
+          # fetch number from version.h in the vehicle folder in the given tag
+          self.repository.git.checkout(tag["tag"])
+          # try to read <vehicle>/version.h
+          path1 = Path(f'{self.repository_path}/{tag["matches"]["name"]}/version.h')
+          path2 = Path(f'{self.repository_path}/{self.valid_name_map[tag["matches"]["name"]]}/version.h')
+          file = path1 if path1.exists() else path2
+          with open(file=file, mode='r') as version_file:
+              content = version_file.read()
+              match = re.search(r'#define\s+FW_MAJOR\s+(\d+)', content)
+              major = int(match.group(1))
+              match = re.search(r'#define\s+FW_MINOR\s+(\d+)', content)
+              minor = int(match.group(1))
+              return major, minor
 
     def clone_repository(self):
         print(f'Starting cloning to: {self.repository_path}')
@@ -41,8 +60,8 @@ class Groundskeeper:
         return git.Repo.clone_from(self.repository_url, self.repository_path)
 
     def run(self):
-        repository = self.clone_repository()
-        tag_names = [tag.path[len('refs/tags/'):] for tag in repository.tags]
+        self.repository = self.clone_repository()
+        tag_names = [tag.path[len('refs/tags/'):] for tag in self.repository.tags]
 
         # Get only valid tag names
         tags = [
@@ -53,11 +72,13 @@ class Groundskeeper:
             } for tag in tag_names if self.tag_regex.search(tag)
         ]
         tags = [tag for tag in tags if self.valid_name_map.get(tag['matches']['name'])]
-
         # Get only the newest patch version
         old_versions = []
         previous_tag = None
         for tag in tags:
+            # Beta releases are unique, we don't need to compare them
+            if tag['matches']['beta']:
+                continue
             if previous_tag:
                 print(f'{previous_tag["tag"]} => {tag["tag"]}')
             if not previous_tag or (
@@ -81,7 +102,7 @@ class Groundskeeper:
         for tag in tags:
             tag_name = tag['tag']
             tag_simple_name = tag['matches']['name']
-            tag_major_version, tag_minor_version = int(tag['matches']['major']), int(tag['matches']['minor'])
+            tag_major_version, tag_minor_version = self.get_version_for_tag(tag)
             vehicle_type = self.valid_name_map.get(tag_simple_name)
             tag_reference = tag['reference']
             folder_name = f'{vehicle_type}-{tag_major_version}.{tag_minor_version}'
@@ -95,7 +116,7 @@ class Groundskeeper:
                 print("Ignoring old version")
                 continue
 
-            repository.git.checkout(tag_reference)
+            self.repository.git.checkout(tag_reference)
             try:
                 subprocess.run([f'{self.repository_path}/Tools/autotest/param_metadata/param_parse.py', '--vehicle', vehicle_type], cwd=self.repository_path)
             except Exception as exception:
